f6b6666e6ae1969646b03f34436fc7811978fdca
kimpors <mikita_korshak@ukr.net>
2022-10-22 19:14:04 +0300

    Додав метод для встановлення випадкових значень у матриці

--- Matrix.cs
+++ Matrix.cs
@@ -1,7 +1,29 @@
-﻿namespace MatrixLib
+﻿using System;
+
+namespace MatrixLib
 {
     public class Matrix
     {
-        
+        public double[][] array;
+        public int XLength { get;}
+        public int YLength { get; }
+
+        public Matrix(int xSize, int ySize)
+        {
+            XLength = xSize;
+            YLength = ySize;
+        }
+
+        public Matrix SetRandomValues(int min = 0, int max = 0)
+        {
+            for (int y = 0; y < YLength; y++)
+            {
+                for (int x = 0; x < XLength; x++)
+                {
+                    array[x][y] = new Random().NextDouble() * (max - min + 1) + min;
+                }
+            }
+            return this;
+        }
     }
 }
