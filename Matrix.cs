cb09e6ac9d657a056c0b3089e9f34868e1b0a036
kimpors <mikita_korshak@ukr.net>
2023-12-22 18:09:23 +0200

    A little of refactoring

--- Matrix.cs
+++ Matrix.cs
@@ -1,221 +1,217 @@
-﻿namespace MatrixLib
+﻿namespace MatrixLib;
+
+public class Matrix
 {
-    public class Matrix
+    private double[,] _array;
+    private const int OPTIMAL_RANK = 2;
+
+    public uint Rows { get; }
+    public uint Columns { get; }
+
+    public Matrix(uint size)
     {
-        private double[,] _array;
-        public uint RowLength { get; }
-        public uint ColumnLength { get; }
-        private const int OPTIMAL_RANK = 2;
+        Rows = Columns = size;
 
-        public Matrix(uint size)
+        _array = new double[size, size];
+    }
+    public Matrix(uint rows, uint columns)
+    {
+        Rows = rows;
+        Columns = columns;
+
+        _array = new double[rows, columns];
+    }
+
+    public Matrix(double[,] array)
+    {
+        Rows = (uint)array.GetLength(0);
+        Columns = (uint)array.GetLength(1);
+
+        _array = (double[,])array.Clone();
+    }
+
+    public static Matrix operator +(Matrix a, Matrix b) => a.Sum(b);
+    public static Matrix operator -(Matrix a, Matrix b) => a.Sub(b);
+    public static Matrix operator *(Matrix a, Matrix b) => a.Mul(b);
+    public static Matrix operator *(Matrix a, double b) => a.Mul(b);
+
+    public static bool IsSquareMatrix(Matrix a)
+        => a.Rows == a.Columns;
+
+    public bool IsEqualSize(Matrix a)
+        => Rows == a.Columns && Columns == a.Columns;
+
+    public static double GetDeterminant(Matrix a)
+    {
+        if (IsSquareMatrix(a) == false)
+            return double.NaN;
+
+        uint size = a.Columns;
+        if (size < OPTIMAL_RANK)
         {
-            RowLength = ColumnLength = size;
-
-            _array = new double[size, size];
+            return 1;
         }
-        public Matrix(uint rows, uint columns)
+        else if (size == OPTIMAL_RANK)
         {
-            RowLength = rows;
-            ColumnLength = columns;
-
-            _array = new double[rows, columns];
+            return a[0, 0] * a[1, 1] - a[1, 0] * a[0, 1];
         }
 
-        public Matrix(double[,] array)
+        double result = 0;
+        Matrix Temp = new Matrix(size - 1);
+
+        for (int n = 0; n < size; n++)
         {
-            RowLength = (uint)array.GetLength(0);
-            ColumnLength = (uint)array.GetLength(1);
-
-            _array = (double[,])array.Clone();
-        }
-
-        public static Matrix operator +(Matrix a, Matrix b)
-            => a.Sum(b);
-        public static Matrix operator -(Matrix a, Matrix b)
-            => a.Sub(b);
-        public static Matrix operator *(Matrix a, Matrix b)
-            => a.Mul(b);
-        public static Matrix operator *(Matrix a, double b)
-            => a.Mul(b);
-
-        public static bool IsSquareMatrix(Matrix a)
-            => a.ColumnLength == a.RowLength;
-
-        public bool IsEqualSize(Matrix a)
-            => RowLength == a.RowLength && ColumnLength == a.ColumnLength;
-
-        public static double GetDeterminant(Matrix a)
-        {
-            if (IsSquareMatrix(a) == false)
-                return double.NaN;
-
-            uint size = a.ColumnLength;
-            if (size < OPTIMAL_RANK)
+            for (int y = 1; y < size; y++)
             {
-                return 1;
-            }
-            else if (size == OPTIMAL_RANK)
-            {
-                return a[0, 0] * a[1, 1] - a[1, 0] * a[0, 1];
-            }
-
-            double result = 0;
-            Matrix Temp = new Matrix(size - 1);
-
-            for (int n = 0; n < size; n++)
-            {
-                for (int y = 1; y < size; y++)
+                for (int x = 0, subx = 0; x < size; x++)
                 {
+                    if (x == n)
+                        continue;
+
+                    Temp[y - 1, subx] = a[y, x];
+                    subx++;
+                }
+            }
+
+            result += a[0, n] * (n % 2 == 0 ? 1 : -1) * GetDeterminant(Temp);
+        }
+
+        return result;
+    }
+
+    public static Matrix GetReverse(Matrix a)
+    {
+        if (double.IsNaN(Matrix.GetDeterminant(a)))
+            return null;
+
+        uint size = a.Columns;
+        double determinant = Matrix.GetDeterminant(a);
+
+        Matrix Result = new Matrix(size);
+        Matrix Temp = new Matrix(size - 1);
+
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                for (int y = 0, suby = 0; y < size; y++)
+                {
+                    if (y == i)
+                        continue;
+
                     for (int x = 0, subx = 0; x < size; x++)
                     {
-                        if (x == n)
+                        if (x == j)
                             continue;
 
-                        Temp[y - 1, subx] = a[y, x];
+                        Temp[suby, subx] = a[y, x];
                         subx++;
                     }
+
+                    suby++;
                 }
 
-                result += a[0, n] * (n % 2 == 0 ? 1 : -1) * GetDeterminant(Temp);
+                Result[j, i] = ((i + j) % 2 == 0 ? 1 : -1) * Matrix.GetDeterminant(Temp);
             }
-
-            return result;
         }
 
-        public static Matrix GetReverse(Matrix a)
+        return Result * Math.Round(1 / determinant, 3);
+    }
+
+    public double this[int row, int column]
+    {
+        get { return _array[row, column]; }
+        set { _array[row, column] = value; }
+    }
+
+    public Matrix SetRandomValues(int min = 0, int max = 0)
+    {
+        Matrix Result = new Matrix(Rows, Columns);
+
+        for (int y = 0; y < Rows; y++)
         {
-            if (double.IsNaN(Matrix.GetDeterminant(a)))
-                return null;
-
-            uint size = a.ColumnLength;
-            double determinant = Matrix.GetDeterminant(a);
-
-            Matrix Result = new Matrix(size);
-            Matrix Temp = new Matrix(size - 1);
-
-            for (int i = 0; i < size; i++)
+            for (int x = 0; x < Columns; x++)
             {
-                for (int j = 0; j < size; j++)
-                {
-                    for (int y = 0, suby = 0; y < size; y++)
-                    {
-                        if (y == i)
-                            continue;
-
-                        for (int x = 0, subx = 0; x < size; x++)
-                        {
-                            if (x == j)
-                                continue;
-
-                            Temp[suby, subx] = a[y, x];
-                            subx++;
-                        }
-
-                        suby++;
-                    }
-
-                    Result[j, i] = ((i + j) % 2 == 0 ? 1 : -1) * Matrix.GetDeterminant(Temp);
-                }
+                Result[y, x] = new Random().NextInt64(min, max);
             }
-
-            return Result * Math.Round(1 / determinant, 3);
         }
 
-        public double this[int row, int column]
-        {
-            get { return _array[row, column]; }
-            set { _array[row, column] = value; }
-        }
+        return Result;
+    }
 
-        public Matrix SetRandomValues(int min = 0, int max = 0)
-        {
-            Matrix Result = new Matrix(RowLength, ColumnLength);
+    public Matrix Sum(Matrix a)
+    {
+        if (IsEqualSize(a) == false)
+            throw new ArgumentException("Don't same size with matrix");
 
-            for (int y = 0; y < RowLength; y++)
+        Matrix Result = new Matrix(a.Rows, a.Columns);
+
+        for (int y = 0; y < Rows; y++)
+        {
+            for (int x = 0; x < Columns; x++)
             {
-                for (int x = 0; x < ColumnLength; x++)
-                {
-                    Result[y, x] = new Random().NextInt64(min, max);
-                }
+                Result[y, x] = this[y, x] + a[y, x];
             }
-
-            return Result;
         }
 
-        public Matrix Sum(Matrix a)
+        return Result;
+    }
+
+    public Matrix Mul(double a)
+    {
+        Matrix Result = new Matrix(Rows, Columns);
+
+        for (int y = 0; y < Rows; y++)
         {
-            if (IsEqualSize(a) == false)
-                throw new ArgumentException("Don't same size with matrix");
-
-            Matrix Result = new Matrix(a.RowLength, a.ColumnLength);
-
-            for (int y = 0; y < RowLength; y++)
+            for (int x = 0; x < Columns; x++)
             {
-                for (int x = 0; x < ColumnLength; x++)
-                {
-                    Result[y, x] = this[y, x] + a[y, x];
-                }
+                Result[y, x] = Math.Round(this[y, x] * a, 3);
             }
-
-            return Result;
         }
 
-        public Matrix Mul(double a)
+        return Result;
+    }
+
+    public Matrix Mul(Matrix a)
+    {
+        if (Columns != a.Rows)
+            throw new ArgumentException("Matrices are inconsistent(ColumnLength != a.RowLength)");
+
+        Matrix Result = new Matrix(Rows, a.Columns);
+
+        for (int y = 0; y < Rows; y++)
         {
-            Matrix Result = new Matrix(RowLength, ColumnLength);
-
-            for (int y = 0; y < RowLength; y++)
+            for (int x = 0; x < a.Columns; x++)
             {
-                for (int x = 0; x < ColumnLength; x++)
+                for (int n = 0; n < Columns; n++)
                 {
-                    Result[y, x] = Math.Round(this[y, x] * a, 3);
+                    Result[y, x] += this[y, n] * a[n, x];
                 }
-            }
 
-            return Result;
+                Result[y, x] = Math.Round(Result[y, x], 3);
+            }
         }
 
-        public Matrix Mul(Matrix a)
+        return Result;
+    }
+
+    public Matrix Sub(Matrix a)
+        => Sum(a.Mul(-1));
+
+    public override string ToString()
+    {
+        string result = string.Empty;
+
+        for (int y = 0; y < Rows; y++)
         {
-            if (ColumnLength != a.RowLength)
-                throw new ArgumentException("Matrices are inconsistent(ColumnLength != a.RowLength)");
-
-            Matrix Result = new Matrix(RowLength, a.ColumnLength);
-
-            for (int y = 0; y < RowLength; y++)
+            for (int x = 0; x < Columns; x++)
             {
-                for (int x = 0; x < a.ColumnLength; x++)
-                {
-                    for (int n = 0; n < ColumnLength; n++)
-                    {
-                        Result[y, x] += this[y, n] * a[n, x];
-                    }
-
-                    Result[y, x] = Math.Round(Result[y, x], 3);
-                }
+                result += this[y, x] + " |";
             }
 
-            return Result;
+            result += "\n";
         }
 
-        public Matrix Sub(Matrix a)
-            => Sum(a.Mul(-1));
-
-        public override string ToString()
-        {
-            string result = string.Empty;
-
-            for (int y = 0; y < RowLength; y++)
-            {
-                for (int x = 0; x < ColumnLength; x++)
-                {
-                    result += this[y, x] + " |";
-                }
-
-                result += "\n";
-            }
-
-            return result;
-        }
+        return result;
     }
 }
